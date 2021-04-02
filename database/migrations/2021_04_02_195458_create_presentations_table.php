bc095f12e4eeb3f96c9bbfadce0033a3cc0588e9
lonefox <cris78geo@gmail.com>
2021-04-02 15:15:33 -0600

    initialized project with breeze

---
+++ database/migrations/2021_04_02_195458_create_presentations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresentationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presentations', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->date('presentation_date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('location');
+            $table->foreignId('author_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presentations');
+    }
+}
