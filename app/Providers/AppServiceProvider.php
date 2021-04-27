cee1cb6de67d5aef8d9e59a7051a50d3a95f5fad
lonefox <cris78geo@gmail.com>
2021-04-26 22:31:14 -0500

    fix index length for mariadb older than 10.2.2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // View::addExtension('html', 'blade');
+        Schema::defaultStringLength(191);
     }
 }
